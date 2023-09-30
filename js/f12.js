10975b0612925dd12ca4ad1bddf64d3cb1b045fc
JiaoXiaohao <2745602379@qq.com>
2023-10-01 04:55:51 +0800

    Site updated: 2023-10-01 04:55:51

---
+++ js/f12.js
@@ -0,0 +1,17 @@
+document.onkeydown = function (e) {
+    // 弹出提示vue
+    new Vue({
+        data: function () {
+            this.$notify({
+                title: "被发现了😜",
+                message: "小伙子，扒源记住要遵循GPL协议！",
+                position: 'top-left',
+                offset: 50,
+                showClose: true,
+                type: "warning",
+                duration: 5000
+            });
+        }
+    })
+    if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) return btf.snackbarShow("你真坏，不能打开控制台喔!"), event.keyCode = 0, event.returnValue = !1, !1
+};
