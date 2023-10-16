c2d954b6a39ce901f9b4ff1b57fd2d55ddb2e92f
JiaoXiaohao <2745602379@qq.com>
2023-10-17 02:48:10 +0800

    Site updated: 2023-10-17 02:48:09

--- js/render.js
+++ js/render.js
@@ -69,11 +69,11 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         //离开当前页面时标签显示内容
-        document.title = '👀跑哪里去了~';
+        document.title = '你快回来~~~';
         clearTimeout(titleTime);
     } else {
         //返回当前页面时标签显示内容
-        document.title = '🐖抓到你啦～';
+        document.title = '欢迎回来~~~';
         //两秒后变回正常标题
         titleTime = setTimeout(function () {
             document.title = OriginTitile;
