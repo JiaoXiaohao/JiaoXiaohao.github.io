23fdc6b236f4610336e28897c5c29ee8d9936e70
JiaoXiaohao <2745602379@qq.com>
2024-01-14 02:03:19 +0800

    Site updated: 2024-01-14 02:03:18

--- js/render.js
+++ js/render.js
@@ -466,25 +466,25 @@ if (window.localStorage.getItem("fpson") == undefined || window.localStorage.get
     document.getElementById("fps").style = "display:none!important"
 }
 // f12.js
-// document.onkeydown = function (e) {
-//     // 判断是否打开控制台
-//     if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) {
-//         new Vue({
-//             data: function () {
-//                 this.$message({
-//                     title: "被发现了😜",
-//                     message: "扒源记住要遵循GPL协议！",
-//                     position: 'top-left',
-//                     offset: 50,
-//                     showClose: true,
-//                     type: "warning",
-//                     duration: 5000
-//                 });
-//             }
-//         })
-//         return false;
-//     }
-// };
+document.onkeydown = function (e) {
+    // 判断是否打开控制台
+    if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) {
+        new Vue({
+            data: function () {
+                this.$message({
+                    title: "被发现了😜",
+                    message: "暂时还没有什么可以看的哦~🤭",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "warning",
+                    duration: 5000
+                });
+            }
+        })
+        return false;
+    }
+};
 
 // chart_2.js
 function switchPostChart() {
